2597cb301cd5b448169bf70365839bb55d5ffc72
lukechn99 <47294899+lukechn99@users.noreply.github.com>
2022-04-30 21:19:05 -0500

    data generation
    + added code to create more training data
    + added code to resize the images to a uniform size and flatten it

--- data.py
+++ data.py
@@ -1,18 +1,35 @@
 from PIL import Image
 import os
 import sys
-
+from filetype import is_image
 
 def generate():
     for filename in os.listdir('train'):
         # don't edit already edited photos
-        if len(filename.split("_")) > 2:
+        if len(filename.split("_")) > 2 or not is_image(filename):
             pass
         img = Image.open('train/' + filename)
         hoz_flip = img.transpose(Image.Transpose.FLIP_LEFT_RIGHT)
         ver_flip = img.transpose(Image.Transpose.FLIP_TOP_BOTTOM)
+        rotate_30 = img.rotate(30)
+        rotate_60 = img.rotate(60)
+        rotate_120 = img.rotate(120)
+        rotate_150 = img.rotate(150)
+        rotate_210 = img.rotate(210)
+        rotate_240 = img.rotate(240)
+        rotate_300 = img.rotate(300)
+        rotate_330 = img.rotate(330)
         hoz_flip.save('train/' + filename.split('.')[0] + '_hoz_flip.jpg')
         ver_flip.save('train/' + filename.split('.')[0] + '_ver_flip.jpg')
+        rotate_30.save('train/' + filename.split('.')[0] + '_rotate_30.jpg')
+        rotate_60.save('train/' + filename.split('.')[0] + '_rotate_60.jpg')
+        rotate_120.save('train/' + filename.split('.')[0] + '_rotate_120.jpg')
+        rotate_150.save('train/' + filename.split('.')[0] + '_rotate_150.jpg')
+        rotate_210.save('train/' + filename.split('.')[0] + '_rotate_210.jpg')
+        rotate_240.save('train/' + filename.split('.')[0] + '_rotate_240.jpg')
+        rotate_300.save('train/' + filename.split('.')[0] + '_rotate_300.jpg')
+        rotate_330.save('train/' + filename.split('.')[0] + '_rotate_330.jpg')
+
 
 def clean():
     for filename in os.listdir('train'):
--- run.py
+++ run.py
@@ -11,44 +11,64 @@ import os
 
 # import images and convert to numpy arrays, in the future we can have all images in a pool and implement a split function to create test/validate/train
 training_images = []
+training_names = []
 for filename in os.listdir('train'):
-    img_data = image.imread('train/' + filename)
-    training_images.append(img_data)
-
-test_images = []
-for filename in os.listdir('test'):
-    img_data = image.imread('test/' + filename)
-    test_images.append(img_data)
+    try:
+        img_data = image.imread('train/' + filename)
+        img_data.resize((400, 400))
+        img_data = img_data.flatten('C')
+        training_images.append(img_data)
+        training_names.append(filename.split('_')[0])
+    except Exception as e:
+        print(e)
 
 valid_images = []
+valid_names = []
 for filename in os.listdir('valid'):
-    img_data = image.imread('valid/' + filename)
-    valid_images.append(img_data)
+    try:
+        img_data = image.imread('valid/' + filename)
+        img_data.resize((400, 400))
+        img_data = img_data.flatten('C')
+        valid_images.append(img_data)
+        valid_names.append(filename.split('_')[0])
+    except Exception as e:
+        print(e)
 
-# generate larger dataset through manipulation of images
-
+test_images = []
+test_names = []
+for filename in os.listdir('test'):
+    try:
+        img_data = image.imread('test/' + filename)
+        img_data.resize((400, 400))
+        img_data = img_data.flatten('C')
+        test_images.append(img_data)
+        test_names.append(filename.split('_')[0])
+    except Exception as e:
+        print(e)
 
 # read in data.
 # training data
-train_data = training_images
-train_labels = np.genfromtxt('train/training_labels.txt',delimiter=',')
+train_data = np.asarray(training_images, dtype=object).astype('int')
+train_labels = np.array(training_names)
+print(train_data.shape)
 
 # validation data
-valid_data = np.genfromtxt("optdigits_valid.txt",delimiter=",")
-valid_x = valid_data[:,:-1]
-valid_y = valid_data[:,-1].astype('int')
+valid_data = np.array(valid_images, dtype=object).astype('int')
+valid_labels = np.array(valid_names)
 
 # test data
-test_data = np.genfromtxt("optdigits_test.txt",delimiter=",")
-test_x = test_data[:,:-1]
-test_y = test_data[:,-1].astype('int')
+# test_data = np.genfromtxt("optdigits_test.txt",delimiter=",")
+# test_x = test_data[:,:-1]
+# test_y = test_data[:,-1].astype('int')
+test_data = np.array(test_images, dtype=object).astype('int')
+test_labels = np.array(test_names)
 
 # normalize the data
 normalizer = Normalization()
 normalizer.fit(train_data)
 train_data = normalizer.normalize(train_data)
-valid_x = normalizer.normalize(valid_x)
-test_x = normalizer.normalize(test_x)
+valid_data = normalizer.normalize(valid_data)
+test_data = normalizer.normalize(test_data)
 
 # process training labels into one-hot vectors
 train_labels = process_label(train_labels)
@@ -61,18 +81,18 @@ for i, num_hid in enumerate(candidate_num_hid):
     # initialize the model
     clf = MLP(num_hid=num_hid)
     # update the model based on training data, and record the best validation accuracy
-    cur_valid_accuracy = clf.fit(train_data,train_labels,valid_x,valid_y)
+    cur_valid_accuracy = clf.fit(train_data,train_labels,valid_data,valid_labels)
     valid_accuracy.append(cur_valid_accuracy)
     print('Validation accuracy for %d hidden units is %.3f' %(candidate_num_hid[i],cur_valid_accuracy))
 
 # select the best number of hidden unit and use it to train the model
 best_num_hid = candidate_num_hid[np.argmax(valid_accuracy)]
 clf = MLP(num_hid=best_num_hid)
-_ = clf.fit(train_x,train_y,valid_x,valid_y)
+_ = clf.fit(train_data,train_labels,valid_data,valid_labels)
 
 # evaluate on test data
-predictions = clf.predict(test_x)
-accuracy = np.count_nonzero(predictions.reshape(-1)==test_y.reshape(-1))/len(test_x)
+predictions = clf.predict(test_data)
+accuracy = np.count_nonzero(predictions.reshape(-1)==test_labels.reshape(-1))/len(test_data)
 
 print('Test accuracy with %d hidden units is %.3f' %(best_num_hid,accuracy))
 
@@ -80,15 +100,15 @@ print('Test accuracy with %d hidden units is %.3f' %(best_num_hid,accuracy))
 ############### Problem b ###################
 # visualization for 2 hidden units
 clf = MLP(num_hid=2)
-_ = clf.fit(train_x,train_y,valid_x,valid_y)
+_ = clf.fit(train_data,train_labels,valid_data,valid_labels)
 # validation set visualization
-hid_features = clf.get_hidden(valid_x)
-plot2d(hid_features,valid_y,'valid')
+hid_features = clf.get_hidden(valid_data)
+plot2d(hid_features,valid_labels,'valid')
 
 
 # visualization for 3 hidden units
 clf = MLP(num_hid=3)
-_ = clf.fit(train_x,train_y,valid_x,valid_y)
+_ = clf.fit(train_data,train_labels,valid_data,valid_labels)
 # validation set visualization
-hid_features = clf.get_hidden(valid_x)
-plot3d(hid_features,valid_y,'valid')
+hid_features = clf.get_hidden(valid_data)
+plot3d(hid_features,valid_labels,'valid')
